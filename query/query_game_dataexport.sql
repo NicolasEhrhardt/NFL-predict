e23934ec5d7a38517eaec7db3470f6e7051b7dec
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2013-11-20 23:12:57 -0800

    use all the years

---
+++ query/query_game_dataexport.sql
@@ -0,0 +1,14 @@
+﻿SELECT 	season_year, 
+	home_team, 
+-- 	home_score, 
+	away_team, 
+-- 	away_score,
+	CASE 
+		WHEN home_score > away_score THEN 1
+		WHEN away_score > home_score THEN -1
+		ELSE 0
+	END AS winning
+       
+FROM 	game
+WHERE 	season_year < 2013 
+	AND season_type = 'Regular';
